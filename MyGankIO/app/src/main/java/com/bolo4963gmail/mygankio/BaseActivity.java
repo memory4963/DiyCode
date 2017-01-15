0b56c253b5905bd915470d66caa6f3f801786d46
luoao <bolo4963@gmail.com>
2017-01-15 14:48:11 +0800

    add MyGankIO into git

---
+++ MyGankIO/app/src/main/java/com/bolo4963gmail/mygankio/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.bolo4963gmail.mygankio;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 10733 on 2017/1/15.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
